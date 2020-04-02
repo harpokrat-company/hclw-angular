df23e2e00390a6e6d4ee5d02e2f271ff63b4accd
Fantin Bibas <fantin.bibas@epitech.eu>
2020-04-02 11:53:15 +0100

    Add module

---
+++ projects/hclw/src/lib/hclw.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HclwComponent } from './hclw.component';
+
+
+
+@NgModule({
+  declarations: [HclwComponent],
+  imports: [
+  ],
+  exports: [HclwComponent]
+})
+export class HclwModule { }
--- projects/hclw/src/public-api.ts
+++ projects/hclw/src/public-api.ts
@@ -3,5 +3,6 @@
  */
 
 export * from './lib/hclw.service';
+export * from './lib/hclw.module';
 export * from './lib/models/secret.model';
 export * from './lib/models/user.model';
