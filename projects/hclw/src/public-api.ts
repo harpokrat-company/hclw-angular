0a4e852db648c46fe2070c6450c80fa4e5b77701
Fantin Bibas <fantin@bib.as>
2020-09-09 20:13:57 +0200

    Export all classes in public-api

--- projects/hclw/src/public-api.ts
+++ projects/hclw/src/public-api.ts
@@ -4,5 +4,10 @@
 
 export * from './lib/hclw.service';
 export * from './lib/hclw.module';
+export * from './lib/models/asecret.model';
 export * from './lib/models/password.model';
+export * from './lib/models/rsakeypair.model';
+export * from './lib/models/rsaprivatekey.model';
+export * from './lib/models/rsapublickey.model';
+export * from './lib/models/symmetrickey.model';
 export * from './lib/models/user.model';
